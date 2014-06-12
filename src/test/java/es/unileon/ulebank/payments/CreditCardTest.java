622433f54ad5439178c562f43e8f998b5ff34d28
isra <isra@192.168.1.183>
2014-06-12 19:57:30 +0200

    Some warnigs fixed

--- src/test/java/es/unileon/ulebank/payments/CreditCardTest.java
+++ src/test/java/es/unileon/ulebank/payments/CreditCardTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
