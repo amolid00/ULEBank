0eed038b38995e08606dd4776d7e4d2de54ac58d
claminaw69 <adriancabello2@gmail.com>
2014-06-14 14:54:57 +0200

    more handler modified

--- src/main/java/es/unileon/ulebank/brokerage/buyable/EnterpriseHandler.java
+++ src/main/java/es/unileon/ulebank/brokerage/buyable/EnterpriseHandler.java
@@ -1,15 +1,24 @@
 package es.unileon.ulebank.brokerage.buyable;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 import es.unileon.ulebank.handler.Handler;
 import es.unileon.ulebank.handler.MalformedHandlerException;
 
-public class EnterpriseHandler implements Handler{
+public class EnterpriseHandler implements Handler, Serializable{
 
-    private String ticker;
+    private String id;
+    
+    private EnterpriseHandler(){
+        
+    }
     
     public EnterpriseHandler(String ticker) throws MalformedHandlerException {
         if(ticker.length() < 6){
-            this.ticker = ticker;
+            this.id = ticker;
         }else{
             throw new MalformedHandlerException("Invalid ticker");
         }
@@ -18,5 +27,22 @@ public class EnterpriseHandler implements Handler{
     @Override
     public int compareTo(Handler another) {
         return toString().compareTo(another.toString());
-    }    
+    }
+
+    @Override
+    @Id
+    @Column(name = "id", unique = true, nullable = false, length = 64)
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+        
+    }
+    
+    public String toString(){
+        return this.id;
+    }
 }
